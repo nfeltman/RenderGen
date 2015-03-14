93b1149aa6989540ec8a5ed4dce701b657a865a2
Nicolas Feltman <nfeltman@gmail.com>
2015-03-13 22:37:55 -0400

    changed build system to use single file

--- sematics/smallstep/makepix.py
+++ sematics/smallstep/makepix.py
@@ -1,16 +1,29 @@
 import re
+import bisect
 from subprocess import call
 
-f = open('bits.tex','r')
+f = open('presentationbits.tex','r')
 s = f.read()
 f.close()
 
-p = re.compile (r"\\bit{(.*?)}\s*%beginbit(.*?)%endbit", re.DOTALL)
+lits = re.finditer (r"%beginlit(.*?)%endlit", s, re.DOTALL)
+bits = re.finditer (r"%beginbit(.*?)%endbit", s, re.DOTALL)
+
+litdict = {l.start() : l.group(1) for l in lits}
+
 k = 0
-for (name,bit) in p.findall (s):
-	bitfile = open ('outputs/bit.tex','w')
-	bitfile.write(bit)
+for bit in bits:
+	name = str(k)
+	k = k + 1
+	fullfile = litdict.copy()
+	fullfile[bit.start()] = bit.group(1)
+
+
+	bitfile = open ('temp.tex','w')
+	for (loc,text) in iter(sorted(fullfile.items())):
+		bitfile.write(text)	
 	bitfile.close()
-	call (['latex', 'presentationbits.tex'])
-	call (['dvipng', '-T', 'tight', '-D', '480', '-Q', '7', '-o', 'temp.png', 'presentationbits.dvi'])
+	
+	call (['latex', 'temp.tex'])
+	call (['dvipng', '-T', 'tight', '-D', '480', '-Q', '7', '-o', 'temp.png', 'temp.dvi'])
 	call (['mv', 'temp.png', 'outputs/'+name+'.png'])
